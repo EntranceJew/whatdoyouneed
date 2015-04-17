958471a13dbbdbe49595cc47db4bc680c2d21bbe
EntranceJew <EntranceJew@gmail.com>
2015-04-17 17:20:38 -0400

    Added dumb static file analysis.
    Added a way to reset any trees generated.
    Made the treeMap exclude '@' on generated sources.

--- example/main.lua
+++ example/main.lua
@@ -12,4 +12,15 @@ require 'lib.complex.parts.part1'
 -- the contents of your table
 local tree = whatdoyouneed:getTree()
 
+-- static analyze time
+whatdoyouneed:analyze('main.lua')
+local stree = whatdoyouneed:getStaticTree()
+
+print('dynamic tree:')
 whatdoyouneed:printTree()
+
+print('static tree:')
+whatdoyouneed:printStaticTree()
+
+-- reset the dynamic tree so we can monitor requires that happen exclusively after main loads
+whatdoyouneed:resetTree()
--- whatdoyouneed.lua
+++ whatdoyouneed.lua
@@ -65,6 +65,7 @@ end
 	}
 ]]
 local treeMap = {}
+local staticTreeMap = {} --like the above, but for static files
 
 local whatdoyouneed = {
 	origRequire = require,
@@ -81,8 +82,34 @@ local whatdoyouneed = {
 		{"(.*)%.lua$", function(w)
 			return w:gsub("%.", "/") .. ".lua"
 		end},
-	}
+	},
+	
+	-- tests to check if a line is actually a require line
+	testRequireLine = {
+		[[require *%( *['"](.+)['"] *%)]],
+		[[require *['"](.+)['"] *]],
+	},
 }
+
+-- see if a static require 
+function whatdoyouneed.resolveRequireString(line)
+	local start, stop, cap
+	for _, testPattern in pairs(whatdoyouneed.testRequireLine) do
+		start, stop, cap = string.find(line, testPattern)
+		if start then
+			return cap
+		end
+	end
+	return nil
+end
+
+-- make note that a static require 
+function whatdoyouneed.staticRequire(filename, include)
+	if not staticTreeMap[filename] then
+		staticTreeMap[filename] = {}
+	end
+	table.insert(staticTreeMap[filename], include)
+end
 
 -- translates require paths into actual files, nil if it can't resolve it
 function whatdoyouneed.fixPath(path)
@@ -90,13 +117,14 @@ function whatdoyouneed.fixPath(path)
 		local count
 		path, count = path:gsub(unpack(gsubArgs))
 		if love.filesystem.isFile(path) then
-			return '@'..path
+			return path
 		end
 	end
 	return nil
 end
 
 -- get a file from a require path, uses fixPath above
+-- returns: evaluated path, successful
 function whatdoyouneed.pathToFile(path)
 	local packageParts = string_split(package.path, ';')
 	local cap, count
@@ -105,13 +133,13 @@ function whatdoyouneed.pathToFile(path)
 		if count > 0 then
 			local newpath = whatdoyouneed.fixPath(cap)
 			if love.filesystem.isFile(cap) then
-				return cap
+				return cap, true
 			elseif newpath then
-				return newpath
+				return newpath, true
 			end
 		end
 	end
-	return '?'..path
+	return path, false
 end
 
 -- turns reverse-linear history into a dependency map
@@ -135,6 +163,50 @@ function whatdoyouneed:printTree()
 	print_r( self:getTree() )
 end
 
+function whatdoyouneed:resetTree()
+	for k in pairs (treeMap) do
+		treeMap[k] = nil
+	end
+end
+
+-- the same, but for the static file functions
+function whatdoyouneed:getStaticTree()
+	return staticTreeMap
+end
+
+function whatdoyouneed:printStaticTree()
+	print_r( self:getStaticTree() )
+end
+
+function whatdoyouneed:resetStaticTree()
+	for k in pairs (staticTreeMap) do
+		staticTreeMap[k] = nil
+	end
+end
+
+--check which files require other files, starting from a given file
+function whatdoyouneed:analyze(filename)
+	if love.filesystem.isFile(filename) then
+		local cap
+		for line in love.filesystem.lines(filename) do
+			cap = whatdoyouneed.resolveRequireString(line)
+			if cap then
+				print("analyze debug: ", cap)
+				local newpath, worked = whatdoyouneed.pathToFile(cap)
+				--print("FOUND:",filename,cap,newpath)
+				if love.filesystem.isFile(cap) then
+					self:analyze(cap)
+				elseif worked then
+					self:analyze(newpath)
+				end
+				whatdoyouneed.staticRequire(filename, cap)
+			end
+		end
+	else
+		assert(false, string.format("Tried to analyze a non-existant file: %s", filename))
+	end
+end
+
 -- actually set up the replacement 
 require = function(res)
 	local level = 1
@@ -151,7 +223,11 @@ require = function(res)
 		if not info then break end
 		-- ignore C functions, whatever boot.lua is doing, and ourselves
 		if info.what ~= "C"  and info.source ~= "boot.lua" and info.source ~= _this_file_path then
-			table.insert(stack, {source=info.source, line=info.currentline})
+			local nsource = info.source
+			if string.sub(nsource, 1, 1) == "@" then
+				nsource = string.sub(nsource, 2)
+			end
+			table.insert(stack, {source=nsource, line=info.currentline})
 		end
 		level = level + 1
 	end
